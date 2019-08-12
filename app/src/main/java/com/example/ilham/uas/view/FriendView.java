1729efbe5f19987d56a8e3e646cab79806a0711d
ilhamMaulanap <ilhammaulanpratama@gmail.com>
2019-08-12 21:39:26 +0700

    project uas

---
+++ app/src/main/java/com/example/ilham/uas/view/FriendView.java
@@ -0,0 +1,9 @@
+package com.example.ilham.uas.view;
+
+//Nama  : Ilham Maulana Pratama
+//NIM   : 10116335
+//KLS   : IF 8
+//TGL   : 4 Agustus 2019
+public interface FriendView {
+    void updateUserInfoTextView(String info);
+}
